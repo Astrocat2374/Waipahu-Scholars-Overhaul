8334624f23c65d37ca746fc8b796b3e42103c7b5
Astrocat2374 <sugiyama.evan@gmail.com>
2019-05-08 12:20:24 -1000

    Fixed register and added login function

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -3,14 +3,13 @@ submit.addEventListener('click', function register(e){
     e.preventDefault()
     let email = document.querySelector('#email').value
     let password = document.querySelector('#password').value
-    let gpaGoal = document.querySelector('#gpa').value
     console.log(email)
     console.log(password)
     auth
-        .createUserWithEmailAndPassword(email, password)
-        .then(data => {
-            database.collection('users').doc(data.user.uid).set({gpa: gpaGoal}).then(()=> console.log('Client: ', gpaGoal))
-            let userRef = database.collection('users').doc(data.user.uid)
+        .signInWithEmailAndPassword(email, password)
+    firebase.auth().onAuthStateChanged(function(user) {
+            if (user) {
+            let userRef = database.collection('users').doc(user.uid)
             userRef.get().then(function(doc){
                 if (doc.exists) {
                     console.log("Document Data: ", doc.data());
@@ -18,8 +17,8 @@ submit.addEventListener('click', function register(e){
                     console.log('Unregistered User!!')
                 }
             })
+            } else {
+                console.log('Idiot')
+            }
         })
-        .catch(err => {
-            console.log(err.message);
-        });
 })
