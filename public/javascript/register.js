694f95e7e3e31107294bde931c4d457a7f04e400
Astrocat2374 <sugiyama.evan@gmail.com>
2019-05-15 10:22:08 -1000

    changed to live-databse

--- public/javascript/register.js
+++ public/javascript/register.js
@@ -1,17 +1,38 @@
-let submit = document.querySelector('#submit-button')
-submit.addEventListener('click', function register(e){
-    e.preventDefault()
-    let email = document.querySelector('#email').value
-    let password = document.querySelector('#password').value
-    let gpaGoal = parseInt(document.querySelector('#gpa').value) + 1
-    console.log(email)
-    console.log(password)
-    auth
-        .createUserWithEmailAndPassword(email, password)
-        .then(data => {
-            database.collection('users').doc(data.user.uid).collection('goals').doc('1').set({goalTitle: 'GPA', goal: gpaGoal}).then(()=> console.log('Client: ', gpaGoal))
-        })
-        .catch(err => {
-            console.log(err.message);
-        });
+const button = document.querySelector('#submit-button')
+let email;
+let password;
+let question1;
+let question2;
+
+// This give you access to firebase database
+const database = firebase.database();
+
+button.addEventListener('click', function register(e) {
+  e.preventDefault();
+  email = document.querySelector('#email').value
+  password = document.querySelector('#password').value
+  question1 = document.querySelector('#gpa').value;
+
+  // Allows you to create email & Password
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(email, password)
+    .catch(err => {
+      console.log(err.message);
+    });
+
+  // Allows you to access current user information
+  var userId = firebase.auth().currentUser.uid;
+  console.log('userId', userId);
+
+  // Stores user information plus quiz information in database
+  firebase.database().ref(`users/${userId}`).set({
+    userId: userId,
+    email: email,
+    question1: question1
+    //some more user data
+  });
+
+  localStorage.setItem('userid', userId)
+  window.location.href = './goals.html';
 })
